e28e9668b6198caa5097493770f11dee86fb6b84
YeganaKhalilova <y.khalilova2@newcastle.ac.uk>
2024-10-19 12:19:45 +0400

    modified rating / removed separate endpoint for rating

--- src/main/java/com/backend/ecommercebackend/service/RatingService.java
+++ src/main/java/com/backend/ecommercebackend/service/RatingService.java
@@ -1,9 +1,12 @@
 package com.backend.ecommercebackend.service;
 
-
-import com.backend.ecommercebackend.dto.request.RatingRequest;
+import com.backend.ecommercebackend.dto.request.CommentRequest;
+import com.backend.ecommercebackend.model.product.Comment;
+import com.backend.ecommercebackend.model.product.Product;
+import java.util.List;
 
 public interface RatingService {
 
-  float addRating(RatingRequest ratingRequest);
+  float getAverageRating(CommentRequest commentRequest, List<Comment> comments);
+  float getRating(CommentRequest commentRequest, Product product, Comment comment);
 }
