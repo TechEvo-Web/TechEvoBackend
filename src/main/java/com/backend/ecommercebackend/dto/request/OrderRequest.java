94971ed9e5caf5d060da0c53dcbe8ac18d175270
SarKhann <serxan.babayev.06@gmail.com>
2024-10-24 19:04:14 +0400

    Order updated

--- src/main/java/com/backend/ecommercebackend/dto/request/OrderRequest.java
+++ src/main/java/com/backend/ecommercebackend/dto/request/OrderRequest.java
@@ -1,7 +1,7 @@
-package com.backend.ecommercebackend.model.order;
+package com.backend.ecommercebackend.dto.request;
 
-
-import jakarta.persistence.*;
+import com.backend.ecommercebackend.model.order.OrderItem;
+import jakarta.persistence.Column;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -9,20 +9,13 @@ import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.util.List;
-
-@Table(name = "orders")
-@Entity
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long orderId;
-String token;
+public class OrderRequest {
     int totalPrice;
     String deliveryType;
+    String token;
     @JdbcTypeCode(SqlTypes.JSON)
     @Column(columnDefinition = "jsonb", nullable = false)
-    List<OrderItem> orderItems;
-
+    List<OrderItemRequest> orderItems;
 }
