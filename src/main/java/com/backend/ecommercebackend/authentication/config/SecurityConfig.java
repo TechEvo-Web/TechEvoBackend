bc6331acb29a28f5ef9183b47c9721069581da18
Sadat Mammadly <memedlisedat033@gmail.com>
2024-11-06 15:52:53 +0400

    update user service and controller

--- src/main/java/com/backend/ecommercebackend/authentication/config/SecurityConfig.java
+++ src/main/java/com/backend/ecommercebackend/authentication/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import com.backend.ecommercebackend.authentication.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Role;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
