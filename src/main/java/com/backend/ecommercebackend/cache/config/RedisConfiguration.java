3663f68c00c108ac2b3bc769d95a25bfdffaf8bf
YeganaKhalilova <y.khalilova2@newcastle.ac.uk>
2024-11-12 19:08:33 +0400

    Server updates

--- src/main/java/com/backend/ecommercebackend/cache/config/RedisConfiguration.java
+++ src/main/java/com/backend/ecommercebackend/cache/config/RedisConfiguration.java
@@ -27,7 +27,7 @@ public class RedisConfiguration {
     if (!redisPassword.isEmpty()) {
       configuration.setPassword(redisPassword);
     }
-    LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder().build();
+    LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder().useSsl().build();
     return new LettuceConnectionFactory(configuration, clientConfig);
   }
 
