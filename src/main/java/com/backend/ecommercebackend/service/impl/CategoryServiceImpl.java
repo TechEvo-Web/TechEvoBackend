d0c3e88a2b0ae2038dee5e0a816366ee95600c11
Sadat Mammadly <memedlisedat033@gmail.com>
2024-11-17 13:53:46 +0400

    filter update

--- src/main/java/com/backend/ecommercebackend/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/backend/ecommercebackend/service/impl/CategoryServiceImpl.java
@@ -57,7 +57,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public Object getFiltersByCategoryName(String categoryName) {
+    public Map<String, Set<Object>> getFiltersByCategoryName(String categoryName) {
         List<String> filterNames = specificationService.getFilterSpecificationsByCategoryName(categoryName);
         List<Product> products = productService.getProductsByCategoryName(categoryName);
         List<String> objectTypeSpecifications = new ArrayList<>();
@@ -69,6 +69,7 @@ public class CategoryServiceImpl implements CategoryService {
         for (Product product : products) {
             for (String filterName : filterNames) {
                 objectTypeSpecifications = specificationRepository.findBySpecificationName(filterName);
+                System.out.println(objectTypeSpecifications);
                 if (objectTypeSpecifications.contains("Object")) {
                     Object filterObjectValue = product.getSpecifications().get(filterName);
                     if (filterObjectValue instanceof Map) {
@@ -76,11 +77,13 @@ public class CategoryServiceImpl implements CategoryService {
                         for (String value : filterValue.keySet())
                             filters.get(filterName).add(value);
                     }
-                } else {
-                    filters.put(filterName, new HashSet<>());
+                }
+                else {
                     Object filterValue = product.getSpecifications().get(filterName);
                     if (filterValue != null) {
-                        filters.get(filterName).add(filterValue);
+//                        filters.get(filterName).add(filterValue);
+                        filters.computeIfAbsent(filterName, k->new HashSet<>()).add(filterValue);
+
                     }
                 }
             }
