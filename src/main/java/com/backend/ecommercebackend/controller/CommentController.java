d812ed7c867aacbf1b0158952dffff33aaa1a0be
Sadat Mammadly <memedlisedat033@gmail.com>
2024-11-26 21:22:08 +0400

    added getAll comment method.

--- src/main/java/com/backend/ecommercebackend/controller/CommentController.java
+++ src/main/java/com/backend/ecommercebackend/controller/CommentController.java
@@ -18,6 +18,10 @@ import java.util.List;
 public class CommentController {
     private final CommentService service;
 
+    @GetMapping("/getAll")
+    public ResponseEntity<List<Comment>> getAll() {
+        return ResponseEntity.ok(service.getAllComments());
+    }
     @GetMapping("/{productId}")
     public ResponseEntity<List<Comment>> getCommentsByProductId(@PathVariable Long productId) {
         return ResponseEntity.ok(service.getAllCommentsByProductId(productId));
